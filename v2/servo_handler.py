7152959a57be6fa86dc37b28c41af68d31008963
hanneskiene <hannes-kiene@live.de>
2019-08-28 00:19:25 +0200

    pulldown; slow open close

--- v2/servo_handler.py
+++ v2/servo_handler.py
@@ -10,24 +10,28 @@ class ServoHandler:
     def __init__(self):
         print("Servo Init")
         gpio.setmode(gpio.BCM)
-        gpio.setup(self.pin, gpio.OUT)
+        gpio.setup(self.pin, gpio.OUT, pull_up_down=GPIO.PUD_DOWN)
         self.p = gpio.PWM(self.pin, 50)
         self.p.start(2.5)
         self.idle()
 
     def idle(self):
-        time.sleep(1) # wait till servo in position
+        time.sleep(0.1) # wait till servo in position
         self.p.ChangeDutyCycle(0)
 
     def open(self, t = 10):
         print("Servo open")
-        self.p.ChangeDutyCycle(9) #2.5 to 12.5
+        for i in range(7):
+            self.p.ChangeDutyCycle(2.5+i) #2.5 to 12.5
+            time.sleep(0.5)
         self.idle()
         #Thread(target=self.closeIn, args=[t]).start()
 
     def close(self):
         print("Servo close")
-        self.p.ChangeDutyCycle(2.5)
+        for i in range(7):
+            self.p.ChangeDutyCycle(8.5-i) #2.5 to 12.5
+            time.sleep(0.5)
         self.idle()
 
     def __del__(self):
